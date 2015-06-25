e5b2a60f98afedaeb8a21d8865ca75301658d329
kwangmyung.kim <kwangmyung.kim@yogiyo.co.kr>
2015-06-25 16:14:12 +0900

    1. 클래스에서 페이지의 엘리먼트를 private으로 변경해 캡슐화 한다. Webdriver 클래스의 인스턴스 변수와 페이지 URL 문자열 변수를 추가한다.

--- Selenium-Maven/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
+++ Selenium-Maven/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
@@ -1,15 +1,18 @@
 package seleniumcookbook.tests.pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class BmiCalcPage {
-	public WebElement heightCMS;
-	public WebElement weightKg;
-	public WebElement Calculate;
-	public WebElement bmi;
-	public WebElement bmi_category;
+	private WebElement heightCMS;
+	private WebElement weightKg;
+	private WebElement Calculate;
+	private WebElement bmi;
+	private WebElement bmi_category;
+	private WebDriver driver;
+	private String url = "http://dl.dropbox.com/u/55228056/bmicalculator.html";
 	
 	public BmiCalcPage(WebDriver driver)
 	{
