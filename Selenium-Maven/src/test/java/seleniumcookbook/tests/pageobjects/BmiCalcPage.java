719ca994d5c1a98c79817b536327b00cfe209383
kwangmyung.kim <kwangmyung.kim@yogiyo.co.kr>
2015-06-25 14:09:50 +0900

    name 변경

---
+++ Selenium-Maven/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
@@ -0,0 +1,9 @@
+package seleniumcookbook.tests.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class BmiCalcPage {
+
+}
