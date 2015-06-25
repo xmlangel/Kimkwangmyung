e1bf68629e42cf3818f646ea57178af6d836327d
kwangmyung.kim <kwangmyung.kim@yogiyo.co.kr>
2015-06-25 14:20:07 +0900

    3. 앞서 생성한 클래스에서 BMI 계산기 페이지의 엘리먼트를 인스턴스 변수로 정의한다. name이나 id 속성을 변수명으로 사용한다.
            public WebElement heightCMS ;
            public WebElement weightKg ;
            public WebElement Calculate ;
            public WebElement bmi ;
            public WebElement bmi_category ;

--- Selenium-Maven/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
+++ Selenium-Maven/src/test/java/seleniumcookbook/tests/pageobjects/BmiCalcPage.java
@@ -5,5 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class BmiCalcPage {
-
+	public WebElement heightCMS;
+	public WebElement weightKg;
+	public WebElement Calculate;
+	public WebElement bmi;
+	public WebElement bmi_category;
 }
